bf723b7e0dcf352166c406029efd9a3a9a17c807
avadtran <89544164+avadtran@users.noreply.github.com>
2021-09-08 10:29:38 -0700

    Created constructor and asciiTable

--- src/LZWCompressor.java
+++ src/LZWCompressor.java
@@ -1,4 +1,29 @@
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class LZWCompressor {
-
+	
+	//creating variable fileName for .txt file needed to be compressed
+	String fileName = "";
+	public LZWCompressor(String inputFileName)
+	{
+		this.fileName = inputFileName;
+	}
+	
+	//creating Dictionary asciiTable
+	public static HashMap<String, Integer> asciiTable;
+	
+	//adding existing characters to asciiIable
+	{
+		for (int i = 0; i < 256; i++)
+		{
+			asciiTable.put((""+((char)i)), i);
+		}
+	}
+	
+	
+	
+	
+	
 }
